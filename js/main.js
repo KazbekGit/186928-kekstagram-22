8baf5d6a3d39a62978e420fd9ad8df9f5b9ee480
Kazbek <Kazbekgit@gmail.com>
2021-02-02 00:11:51 +0300

    Нужно больше функций

---
+++ js/main.js
@@ -0,0 +1,19 @@
+/* https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random */
+function getRandomIntInclusive(min, max) {
+  if (min < 0) min = 0;
+  if (max < 0) max = 0;
+  if(min > max) {
+    let temp = min;
+    min = max;
+    max = temp;
+  }
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min; //Максимум и минимум включаются
+}
+
+let checkStringLength = function(userString, maxLength){
+  if (userString.length > maxLength) return false;
+  return true;
+}
+
