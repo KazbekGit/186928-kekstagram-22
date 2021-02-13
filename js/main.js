aaee3cb92d8843a7887939ffb9301db23a5a3659
Kazbek <kazbekgit@gmail.com>
2021-02-13 11:40:59 +0300

    module3-task1

--- js/main.js
+++ js/main.js
@@ -1,4 +1,3 @@
-/* https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random */
 function getRandomIntInclusive(min, max) {
   if (min < 0) min = 0;
   if (max < 0) max = 0;
@@ -17,6 +16,38 @@ let checkStringLength = function(userString, maxLength){
   return true;
 }
 
+let ARRAY_LENGTH = 25;
+let INITIAL_VALUE = 1;
+let END_VALUE = 25;
+
 getRandomIntInclusive(100, 50);
 checkStringLength('JScript', 5);
+
+let usedValues = [];
+let getUniqueValue = function(){
+  let currentValue = getRandomIntInclusive(INITIAL_VALUE, END_VALUE);
+  usedValues.forEach(element => {
+    if(element === currentValue) getUniqueValue();
+  });
+  usedValues.push(currentValue);
+  return currentValue;
+}
+
+function PhotoDescrition() {
+  this.id = getUniqueValue();
+  this.url = `photos/${getUniqueValue()}.jpg`
+  this.description = ['Массив описания'];
+  this.likes = getRandomIntInclusive(15, 200);
+  this.comments = [{},{},{}];
+}
+
+let PhotoDescritionArray = [];
+for (let index = 0; index < ARRAY_LENGTH; index++) {
+  PhotoDescritionArray[index] = new PhotoDescrition();
+}
+
+PhotoDescritionArray.forEach(element => {
+  console.log(element.id);
+  console.log(element.url);
+});
 
