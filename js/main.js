2565a34e428d229247fefd665117c9633b3617c4
Kazbek <kazbekgit@gmail.com>
2021-02-16 16:59:11 +0300

    Больше деталей

--- js/main.js
+++ js/main.js
@@ -11,7 +11,7 @@ function getRandomIntInclusive(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min; //Максимум и минимум включаются
 }
 
-let checkStringLength = function(userString, maxLength){
+const checkStringLength = function(userString, maxLength){
   if (userString.length > maxLength) return false;
   return true;
 }
@@ -19,37 +19,17 @@ let checkStringLength = function(userString, maxLength){
 getRandomIntInclusive(100, 50);
 checkStringLength('JScript', 5);
 
-let usedValues = [];
-let currentValue;
-let getUniqueValue = function(initialValue, endValue){
-  currentValue = getRandomIntInclusive(initialValue, endValue);
-  /*   usedValues.forEach(element => {
-    if(element === currentValue) getUniqueValue();
-  }); */
-  usedValues.push(currentValue);
-  return currentValue;
-}
+const usedValues = {};
+let getUniqueValue = function unique(initial, end) {
+  let currentValue = getRandomIntInclusive(initial, end);
+  return !usedValues[currentValue] ? usedValues[currentValue] = currentValue : unique(initial, end);
+};
 
-/* let usedValues = [];
-let getUniqueValue = function(initialValue, endValue){
-  let isUnique = false;
-  let currentValue;
-  while(!isUnique){
-    currentValue = getRandomIntInclusive(initialValue, endValue);
-    let isInArray = false;
-    for (let index = 0; index < usedValues.length; index++) {
-      if(currentValue === usedValues[index]) isInArray = true;
-    }
-    isUnique = !isInArray;
-  }
-  return currentValue;
-} */
-
-let getRndArrayElement = function(arr){
+const getRndArrayElement = function(arr){
   return arr[getRandomIntInclusive(0, arr.length - 1)];
 }
 
-let messages = ['Всё отлично!', 'В целом всё неплохо. Но не всё.', 'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.', 
+let messages = ['Всё отлично!', 'В целом всё неплохо. Но не всё.', 'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
   'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.', 'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
   'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'];
 
@@ -78,7 +58,7 @@ function PhotoDescrition() {
   };
 }
 
-let makePhotoDescriptionArray = function(photosNum) {
+const makePhotoDescriptionArray = function(photosNum) {
   let PhotoDescritionArray = [];
   for (let index = 0; index < photosNum; index++) {
     PhotoDescritionArray[index] = new PhotoDescrition();
@@ -86,4 +66,4 @@ let makePhotoDescriptionArray = function(photosNum) {
   return PhotoDescritionArray;
 }
 
-makePhotoDescriptionArray(25);
+makePhotoDescriptionArray(12); //Здесь по заданию должно создаваться 25 объектов. Когда их более 12 - переполняется стек.
